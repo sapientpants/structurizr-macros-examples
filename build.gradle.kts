aa6cac2936271eb7cbbdca0247ba4912dd31232e
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-03 22:55:46 +0200

    Remove jacoco dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "io.github.sapientpants"
-version = "1.0-SNAPSHOT"
+version = "0.0.1-SNAPSHOT"
 
 application {
     mainClassName = "io.sapientpants.structurizr.macros.example.MainKt"
