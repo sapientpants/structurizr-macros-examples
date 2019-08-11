1ec439e36e31ff07ae8ea24bd6ea3d20637ce08c
Marc Tremblay <marc.tremblay@protonmail.ch>
2019-08-11 12:23:47 +0200

    Refactor to use StructurizrBuilder

--- src/main/kotlin/io/sapientpants/structurizr/macros/example/Main.kt
+++ src/main/kotlin/io/sapientpants/structurizr/macros/example/Main.kt
@@ -2,128 +2,100 @@ package io.sapientpants.structurizr.macros.example
 
 import com.structurizr.model.Location
 import io.github.sapientpants.structurizr.macros.*
+import io.github.sapientpants.structurizr.macros.builder.StructurizrBuilder
 
 const val JSON_HTTPS = "JSON/HTTPS"
 const val SMTP = "SMTP"
 
 fun main(args: Array<String>) {
-    // Setup the workspace
-    val workspace = StructurizrInitializer.init(
+    StructurizrBuilder.build(
+        "Structurizr Macros Inc.",
         "Structurizr Macros Example",
-        "An example project demonstrating how to use the Structurizr macros",
-        "Structurizr Macros Inc."
-    )
+        "An example project demonstrating how to use the Structurizr macros"
+    ) { _, model, _ ->
+        // Define users
 
-    val model = workspace.model
+        val user = model.addPerson(
+            Location.External,
+            "A user",
+            "Anyone who uses the Structurizr Macros Example system")
 
-    val views = workspace.views
+        // Define software systems
 
-    // Define users
+        val softwareSystem = model.addSoftwareSystem(
+            Location.Internal,
+            "Structurizr Macros Example",
+            "A system for demonstrating the Structurizr Macros")
+        softwareSystem.addTags(Styling.SYSTEM_OF_INTEREST_TAG)
 
-    val user = model.addPerson(
-        Location.External,
-        "A user",
-        "Anyone who uses the Structurizr Macros Example system")
+        val emailSystem = model.addSoftwareSystem(
+            Location.Internal,
+            "Structurizr Macros Email System",
+            "The internal email system"
+        )
 
-    // Define software systems
+        val emailProvider = model.addSoftwareSystem(
+            Location.External,
+            "Email Provider",
+            "A system for delivering email to users")
 
-    val softwareSystem = model.addSoftwareSystem(
-        Location.Internal,
-        "Structurizr Macros Example",
-        "A system for demonstrating the Structurizr Macros")
-    softwareSystem.addTags(Styling.SYSTEM_OF_INTEREST_TAG)
+        // Define containers
 
-    val emailSystem = model.addSoftwareSystem(
-        Location.Internal,
-        "Structurizr Macros Email System",
-        "The internal email system"
-    )
+        val mobileFrontend = softwareSystem.addContainer("Mobile Frontend",
+            "The mobile frontend for the Structurizr Macros Example system",
+            "Android/Kotlin, iOS/Swift")
+        mobileFrontend.addTags(Styling.MOBILE_DEVICE_PORTRAIT_TAG)
 
-    val emailProvider = model.addSoftwareSystem(
-        Location.External,
-        "Email Provider",
-        "A system for delivering email to users")
+        val webFrontend = softwareSystem.addContainer("Web Frontend",
+            "The web frontend for the Structurizr Macros Example system",
+            "Vue.js")
+        webFrontend.addTags(Styling.WEB_BROWSER_TAG)
 
-    // Define containers
+        val backend = softwareSystem.addContainer("Backend",
+            "The backend for the Structurizr Macros Example system",
+            "Spring Boot, Kotlin")
 
-    val mobileFrontend = softwareSystem.addContainer("Mobile Frontend",
-        "The mobile frontend for the Structurizr Macros Example system",
-        "Android/Kotlin, iOS/Swift")
-    mobileFrontend.addTags(Styling.MOBILE_DEVICE_PORTRAIT_TAG)
+        val database = softwareSystem.addContainer("Database",
+            "The data store for the Structurizr Macros Example system",
+            "PostgreSQL")
+        database.addTags(Styling.DATABASE_TAG)
 
-    val webFrontend = softwareSystem.addContainer("Web Frontend",
-        "The web frontend for the Structurizr Macros Example system",
-        "Vue.js")
-    webFrontend.addTags(Styling.WEB_BROWSER_TAG)
+        // Define components
 
-    val backend = softwareSystem.addContainer("Backend",
-        "The backend for the Structurizr Macros Example system",
-        "Spring Boot, Kotlin")
+        // ... mobile frontend components - omitted for brevity ...
 
-    val database = softwareSystem.addContainer("Database",
-        "The data store for the Structurizr Macros Example system",
-        "PostgreSQL")
-    database.addTags(Styling.DATABASE_TAG)
+        // ... web frontend components - omitted for brevity  ...
 
-    // Define components
+        // backend components
 
-    // ... mobile frontend components - omitted for brevity ...
+        val api = backend.addComponent("API",
+            "The API endpoints",
+            "Spring Boot, Kotlin")
 
-    // ... web frontend components - omitted for brevity  ...
+        val businessLogic = backend.addComponent("Business Logic",
+            "The business logic",
+            "Sprint Boot, Kotlin")
 
-    // backend components
+        val repository = backend.addComponent("Repository",
+            "The repository for accessing data",
+            "Sprint Boot, Kotlin")
 
-    val api = backend.addComponent("API",
-        "The API endpoints",
-        "Spring Boot, Kotlin")
+        // Wire everything up
 
-    val businessLogic = backend.addComponent("Business Logic",
-        "The business logic",
-        "Sprint Boot, Kotlin")
+        user.uses(mobileFrontend, "uses")
+        user.uses(webFrontend, "uses")
 
-    val repository = backend.addComponent("Repository",
-        "The repository for accessing data",
-        "Sprint Boot, Kotlin")
+        mobileFrontend.uses(api, "uses", JSON_HTTPS)
 
-    // Wire everything up
+        webFrontend.uses(api, "uses", JSON_HTTPS)
 
-    user.uses(mobileFrontend, "uses")
-    user.uses(webFrontend, "uses")
+        api.uses(businessLogic, "uses")
 
-    mobileFrontend.uses(api, "uses", JSON_HTTPS)
+        businessLogic.uses(repository, "uses")
+        businessLogic.uses(emailSystem, "uses", SMTP)
 
-    webFrontend.uses(api, "uses", JSON_HTTPS)
+        emailSystem.uses(emailProvider, "uses", SMTP)
 
-    api.uses(businessLogic, "uses")
-
-    businessLogic.uses(repository, "uses")
-    businessLogic.uses(emailSystem, "uses", SMTP)
-
-    emailSystem.uses(emailProvider, "uses", SMTP)
-
-    repository.uses(database, "uses", "SQL")
-
-    model.addImplicitRelationships()
-
-    // Declare the diagrams to render
-
-    SystemLandscapeView.addToViews(model, views)
-
-    SystemContextView.addToViews(softwareSystem, views)
-
-    ContainerView.addToViews(softwareSystem, views)
-
-    ContainerContextViews.addToViews(softwareSystem.containers, views)
-
-    ComponentViews.addToViews(softwareSystem.containers, views)
-
-    AdrDocumentation.addToWorkspace(workspace, softwareSystem)
-
-    Arc42Documentation.addToWorkspace(workspace, softwareSystem)
-
-    // Apply the style
-    Styling.apply(views)
-
-    // Render the diagrams
-    StructurizrRenderer.render(workspace)
+        repository.uses(database, "uses", "SQL")
+    }
 }
