83a54f017d48bfc2b475ce2a1d7d7669812f3312
Marc Tremblay <marc.tremblay@protonmail.ch>
2020-07-22 21:45:33 +0200

    Upgrade to 0.0.35

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
-    implementation("io.github.sapientpants:structurizr-macros:0.0.34")
+    implementation("io.github.sapientpants:structurizr-macros:0.0.35")
 }
 
 tasks.withType<KotlinCompile> {
